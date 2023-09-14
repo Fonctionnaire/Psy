123368ab0ddab9ecab1cee2fbbfa877380639300
Fonctionnaire <thibaut.gruffy@gmail.com>
2023-09-14 16:36:08 +0200

    Feat(asset): install Bootstrap via Asset Mapper

---
+++ src/Controller/Front/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Front;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/', name: 'app_home')]
+class HomeController extends AbstractController
+{
+
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('front/home/index.html.twig', []);
+    }
+}
